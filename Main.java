c0ef58650e8cfc13c6becef3ce98569e80f82772
Joshua Nobleza <joshnobleza@bell.net>
2023-07-05 11:44:33 -0400

    feat: Check whether a number is a Palindromic one

--- Main.java
+++ Main.java
@@ -3,31 +3,57 @@ package numbers;
 import java.util.Scanner;
 public class Main {
 
-    public static void showNumProperties(int num) {
+    public static boolean isPalindromic(long num) {
+        String strNum = Long.toString(num);
+        StringBuilder reversedNum = new StringBuilder();
+
+        for (int i = strNum.length() - 1; i >= 0; i--) {
+            reversedNum.append(strNum.charAt(i));
+        }
+
+        return strNum.contentEquals(reversedNum);
+    }
+
+    public static void showNumProperties(long num) {
         boolean even = num % 2 == 0;
         boolean odd = num % 2 != 0;
         boolean buzz = num % 7 == 0 || num % 10 == 7;
-        boolean duck = Integer.toString(num).contains("0");;
+        boolean duck = Long.toString(num).contains("0");
+        boolean palindromic = isPalindromic(num);
 
         System.out.printf("""
-                Properties of %d
-                    even: %b
-                    odd: %b
-                    buzz: %b
-                    duck: %b
-                """, num, even, odd, buzz, duck);
+                %nProperties of %d
+                        even: %b
+                        odd: %b
+                        buzz: %b
+                        duck: %b
+                palindromic: %b
+                %n""", num, even, odd, buzz, duck, palindromic);
     }
 
     public static void main(String[] args) {
-        System.out.println("Enter a natural number:");
+        System.out.println("Welcome to Amazing Numbers!");
+        System.out.printf("""
+                %nSupported requests:
+                - enter a natural number to know its properties;
+                - enter 0 to exit.
+                """);
 
         Scanner scanner = new Scanner(System.in);
 
-        int num = scanner.nextInt();
-        if (num > 0) {
-            showNumProperties(num);
-        } else {
-            System.out.println("This number is not natural!");
+        while (true) {
+            System.out.print("Enter a request: ");
+
+            long num = scanner.nextLong();
+
+            if (num > 0) {
+                showNumProperties(num);
+            } else if (num == 0) {
+                System.out.printf("%nGoodbye!%n");
+                break;
+            } else {
+                System.out.printf("%nThe first parameter should be a natural number or zero.%n%n");
+            }
         }
     }
 }
