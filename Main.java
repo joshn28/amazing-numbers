fc9dc7f61a901fdadd2811e9dba6aae99fdf48c8
Joshua Nobleza <joshnobleza@bell.net>
2023-07-06 15:07:23 -0400

    feat: Add gapful property and process two numbers
    Add functionality to check if a number is a gapful one and allow the
    user to enter two numbers to obtain the properties of a list of numbers.

--- Main.java
+++ Main.java
@@ -3,6 +3,21 @@ package numbers;
 import java.util.Scanner;
 public class Main {
 
+    public static boolean isGapful(long num) {
+        String strNum = Long.toString(num);
+
+        if (strNum.length() < 3) {
+            return false;
+        } else {
+            String firstDigit = String.valueOf(strNum.charAt(0));
+            String lastDigit = String.valueOf(strNum.charAt(strNum.length() - 1));
+
+            int combined = Integer.parseInt(firstDigit + lastDigit);
+
+            return num % combined == 0;
+        }
+    }
+
     public static boolean isPalindromic(long num) {
         String strNum = Long.toString(num);
         StringBuilder reversedNum = new StringBuilder();
@@ -14,21 +29,42 @@ public class Main {
         return strNum.contentEquals(reversedNum);
     }
 
+    public static void printInfo(long num) {
+        StringBuilder info = new StringBuilder(num + " is ");
+
+        info.append(num % 2 == 0 ? "even" : "odd");
+        info.append(num % 7 == 0 || num % 10 == 7 ? ", buzz" : "");
+        info.append(Long.toString(num).contains("0") ? ", duck" : "");
+        info.append(isPalindromic(num) ? ", palindromic" : "");
+        info.append(isGapful(num) ? ", gapful" : "");
+
+        System.out.println(info);
+    }
+
+    public static void showNumPropertiesList(long start, long end) {
+        for (long i = 0; i < end; i++) {
+            printInfo(start++);
+        }
+        System.out.println();
+    }
+
     public static void showNumProperties(long num) {
         boolean even = num % 2 == 0;
         boolean odd = num % 2 != 0;
         boolean buzz = num % 7 == 0 || num % 10 == 7;
         boolean duck = Long.toString(num).contains("0");
         boolean palindromic = isPalindromic(num);
+        boolean gapful = isGapful(num);
 
         System.out.printf("""
-                %nProperties of %d
-                        even: %b
-                        odd: %b
+                %nProperties of %,d
                         buzz: %b
                         duck: %b
-                palindromic: %b
-                %n""", num, even, odd, buzz, duck, palindromic);
+                 palindromic: %b
+                      gapful: %b
+                        even: %b
+                         odd: %b
+                %n""", num, buzz, duck, palindromic, gapful, even, odd);
     }
 
     public static void main(String[] args) {
@@ -36,23 +72,43 @@ public class Main {
         System.out.printf("""
                 %nSupported requests:
                 - enter a natural number to know its properties;
+                - enter two natural numbers to obtain the properties of the list:
+                  * the first parameter represents a starting number;
+                  * the second parameter shows how many consecutive numbers are to be printed;
+                - separate the parameters with one space;
                 - enter 0 to exit.
-                """);
+                %n""");
 
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             System.out.print("Enter a request: ");
 
-            long num = scanner.nextLong();
+            String[] nums = scanner.nextLine().split(" ");
 
-            if (num > 0) {
-                showNumProperties(num);
-            } else if (num == 0) {
-                System.out.printf("%nGoodbye!%n");
-                break;
+            if (nums.length > 1) {
+                long firstNum = Long.parseLong(nums[0]);
+                long secondNum = Long.parseLong(nums[1]);
+
+                if (firstNum < 0) {
+                    System.out.printf("%nThe first parameter should be a natural number or zero.%n%n");
+                } else if (secondNum < 0) {
+                    System.out.printf("%nThe second parameter should be a natural number.%n%n");
+                } else {
+                    System.out.println();
+                    showNumPropertiesList(firstNum, secondNum);
+                }
             } else {
-                System.out.printf("%nThe first parameter should be a natural number or zero.%n%n");
+                long num = Long.parseLong(nums[0]);
+
+                if (num > 0) {
+                    showNumProperties(num);
+                } else if (num == 0) {
+                    System.out.printf("%nGoodbye!%n");
+                    break;
+                } else {
+                    System.out.printf("%nThe first parameter should be a natural number or zero.%n%n");
+                }
             }
         }
     }
